5d2918687308207d0cd14142329c400ca62e136a
Emeafu Francis Ebere <ictn.klc.2025@gmail.com>
2025-07-29 14:27:57 +0530

    editing template

--- ictn2025/settings.py
+++ ictn2025/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g2$)(n7-&^rw$k-wmx_#&h@ipt#d6yz!g)f(oh5xabiaebh-cq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ictnklc.org', 'www.ictnklc.org', 'localhost', '127.0.0.1', '139.59.45.199']
  
