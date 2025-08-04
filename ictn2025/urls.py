5e8c58aa74bc688a4018cb3c3e029fb2294374e9
Emeafu Francis Ebere <ictn.klc.2025@gmail.com>
2025-08-04 06:19:29 +0530

    updating template

--- ictn2025/urls.py
+++ ictn2025/urls.py
@@ -23,7 +23,14 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name="base.html"), name="home"),
+    path("", TemplateView.as_view(template_name="home.html"), name="base"),
+    path("home", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("registration", TemplateView.as_view(template_name="registration.html"), name="registration"),
+    path("speakers", TemplateView.as_view(template_name="speakers.html"), name="speakers"),
+    path("sponsorship", TemplateView.as_view(template_name="sponsorship.html"), name="sponsorship"),
+    path("publications", TemplateView.as_view(template_name="publications.html"), name="publications"),
+    path("awards", TemplateView.as_view(template_name="awards.html"), name="awards"),
+    path("organizers", TemplateView.as_view(template_name="organizers.html"), name="organizers"),
 ]
 
 
